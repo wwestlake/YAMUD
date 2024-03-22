81b4d9786b4712369044939b81b17fb79b05a08f
William Westlake <wwestlake@lagdaemon.com>
2024-03-22 17:29:30 -0400

    removed animated square

---
+++ LagDaemon.YAMUD/LagDaemon.YAMUD.WebClient/wwwroot/JavaScript.js
@@ -0,0 +1,26 @@
+﻿// JavaScript functions to interact with Blazor component
+
+// Function to get the width of the canvas
+window.getWidth = function () {
+    var canvas = document.getElementById('myCanvas');
+    return canvas.width;
+};
+
+// Function to get the height of the canvas
+window.getHeight = function () {
+    var canvas = document.getElementById('myCanvas');
+    return canvas.height;
+};
+
+// Function to draw a square on the canvas at the specified position
+window.drawSquare = function (x, y) {
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the square
+    ctx.fillStyle = 'blue';
+    ctx.fillRect(x, y, 50, 50); // Adjust size as needed
+};
