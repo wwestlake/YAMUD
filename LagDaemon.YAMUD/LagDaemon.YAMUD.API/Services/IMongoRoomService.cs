04e9aae66e6f9e45ecac970d391d86c4a789f917
William Westlake <wwestlake@lagdaemon.com>
2024-03-22 18:21:20 -0400

    setting up to store data on mongodb

---
+++ LagDaemon.YAMUD/LagDaemon.YAMUD.API/Services/IMongoRoomService.cs
@@ -0,0 +1,13 @@
+﻿using LagDaemon.YAMUD.Model.Map;
+
+namespace LagDaemon.YAMUD.API.Services
+{
+    public interface IMongoRoomService
+    {
+        Task<Room> CreateRoom(Room room);
+        Task DeleteRoom(Guid id);
+        Task<Room> GetRoomById(Guid id);
+        Task<IEnumerable<Room>> GetRoomsByLevel(int level);
+        Task UpdateRoom(Room room);
+    }
+}
