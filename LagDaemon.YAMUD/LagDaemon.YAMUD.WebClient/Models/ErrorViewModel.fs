db4ae97e0efc48fb9f243b4d818f3d97e155949a
William Westlake <wwestlake@lagdaemon.com>
2024-03-22 13:05:27 -0400

    removed all F# projects

--- LagDaemon.YAMUD/LagDaemon.YAMUD.WebClient/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace LagDaemon.YAMUD.WebClient.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
