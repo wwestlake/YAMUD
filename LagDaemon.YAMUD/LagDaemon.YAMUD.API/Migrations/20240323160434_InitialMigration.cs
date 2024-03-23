1273cabb13aa1e52efd8762da637aeae60d83c22
William Westlake <wwestlake@lagdaemon.com>
2024-03-23 13:28:04 -0400

    update ef

---
+++ LagDaemon.YAMUD/LagDaemon.YAMUD.API/Migrations/20240323160434_InitialMigration.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LagDaemon.YAMUD.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    DisplayName = table.Column<string>(type: "text", nullable: true),
+                    HashedPassword = table.Column<string>(type: "text", nullable: true),
+                    EmailAddress = table.Column<string>(type: "text", nullable: true),
+                    Status = table.Column<int>(type: "integer", nullable: false),
+                    Roles = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+        }
+    }
+}
---
+++ LagDaemon.YAMUD/LagDaemon.YAMUD.API/YamudDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LagDaemon.YAMUD.Model.User;
+
+namespace LagDaemon.YAMUD.API
+{
+    public class YamudDbContext : DbContext
+    {
+        private readonly string connectionString = "Server=localhost;Port=5432;Database=yamud;User Id=postgres;Password=htrv5gpl;";
+
+        public DbSet<UserAccount> UserAccounts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(connectionString);
+        }
+    }
+}
