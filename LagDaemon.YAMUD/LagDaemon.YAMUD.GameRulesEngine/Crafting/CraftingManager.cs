daf4f540374736f0a7a6014aa52de0b63d7fbce7
William Westlake <wwestlake@lagdaemon.com>
2024-04-02 09:22:16 -0400

    updating to crafting system

--- LagDaemon.YAMUD/LagDaemon.YAMUD.GameRulesEngine/Crafting/CraftingManager.cs
+++ LagDaemon.YAMUD/LagDaemon.YAMUD.GameRulesEngine/Crafting/CraftingManager.cs
@@ -1,18 +1,17 @@
-﻿using LagDaemon.YAMUD.Model.Items;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FluentResults;
+using LagDaemon.YAMUD.Model.Items;
 
 namespace LagDaemon.YAMUD.GameRulesEngine.Crafting
 {
-    // TODO: This needs to use the rules, and also needs to return bwtter Result objects with details of failure
+    // TODO: This needs to use the rules, and also needs to return better Result objects with details of failure
 
     public class CraftingManager
     {
-        public bool CraftItem(CraftingRecipe recipe, Dictionary<Item, int> inventory)
+        public Result CraftItem(CraftingRecipe recipe, Dictionary<Item, int> inventory)
+            
         {
+            var errors = new List<string>();
+
             // Step 1: Iterate through each required item in the recipe
             foreach (var requiredItem in recipe.RequiredItems)
             {
@@ -20,9 +19,11 @@ namespace LagDaemon.YAMUD.GameRulesEngine.Crafting
                 if (!inventory.ContainsKey(requiredItem.Item) || inventory[requiredItem.Item] < requiredItem.Quantity)
                 {
                     // Not enough of this item in the inventory, return false
-                    return false;
+                    errors.Add($"Meed more {requiredItem.Item.Name}");
                 }
             }
+
+            if (errors.Count > 0) return Result.Fail(errors);
 
             // Step 3: Inventory has enough of all required items, proceed with crafting
             // Step 4: Create the output item and add it to the inventory
@@ -42,7 +43,7 @@ namespace LagDaemon.YAMUD.GameRulesEngine.Crafting
                 inventory[requiredItem.Item] -= requiredItem.Quantity;
             }
 
-            return true;
+            return Result.Ok();
         }
     }
 }
