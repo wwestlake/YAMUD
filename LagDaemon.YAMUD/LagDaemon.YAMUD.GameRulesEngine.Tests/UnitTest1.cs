877ebe802d57602c86dfdb5fab793fdc714f7753
William Westlake <wwestlake@lagdaemon.com>
2024-04-01 20:42:28 -0400

    removed bogus folder

---
+++ LagDaemon.YAMUD/LagDaemon.YAMUD.GameRulesEngine.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace LagDaemon.YAMUD.GameRulesEngine.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
